b6372818b4c6d9749635e7ba3fb7f27b8d9c08d4
MrJuehang <139036608+MrYueHang@users.noreply.github.com>
2026-07-22 22:39:03 +0200

    feat(desktop): start authorized LICHTREICH app sync

--- components/system/Desktop/index.tsx
+++ components/system/Desktop/index.tsx
@@ -2,12 +2,14 @@ import { memo, useRef } from "react";
 import StyledDesktop from "components/system/Desktop/StyledDesktop";
 import useWallpaper from "components/system/Desktop/Wallpapers/useWallpaper";
 import FileManager from "components/system/Files/FileManager";
+import useLichtreichAppSync from "components/system/Desktop/useLichtreichAppSync";
 import { DESKTOP_PATH } from "utils/constants";
 
 const Desktop: FC = ({ children }) => {
   const desktopRef = useRef<HTMLElement | null>(null);
 
   useWallpaper(desktopRef);
+  useLichtreichAppSync();
 
   return (
     <StyledDesktop ref={desktopRef}>
